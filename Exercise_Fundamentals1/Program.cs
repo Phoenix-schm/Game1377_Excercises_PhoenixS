dd4dc4903b4b5131cc2e6486827769b59a06f5c1
Phoenix-schm <ellaschmitt2016@gmail.com>
2025-01-30 22:17:54 -0600

    Exercise_Fundamentals 1: Added OutputString() of exercise

---
+++ Exercise_Fundamentals1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise_Fundamentals1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OutputStrings();
+        }
+        static void OutputStrings()
+        {
+            // 1.
+            // Declare a string
+            string gameString = "I’m in Game 1377: Scripting for Game Designers";
+
+            Console.WriteLine(gameString);                  // Output string to new line
+            Console.WriteLine(gameString.ToUpper());        // Output string in upper case
+            Console.WriteLine(gameString.ToLower());        // Output string in lower case
+            Console.WriteLine(gameString.Substring(8));     // Output string starting at index 8
+            Console.WriteLine(gameString.Substring(8, 3));  // Output string from index 8 to 11
+            Console.WriteLine(gameString.Substring(8, 1));  // Output only index 8 string
+
+        }
+    }
+}
