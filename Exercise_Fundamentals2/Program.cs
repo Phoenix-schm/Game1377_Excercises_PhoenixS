e5649fd35b484a268385d3bd2e04bc97cc3c1a47
Phoenix-schm <ellaschmitt2016@gmail.com>
2025-02-05 18:59:35 -0600

    - Added Exercise_Fundamentals2 project - Finished Part 1 of assignment in SwitchStatement()

---
+++ Exercise_Fundamentals2/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Exercise_Fundamentals2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SwitchStatement();
+        }
+
+        static void SwitchStatement()
+        {
+            // variable declarations
+            // Const strings for possible future changes
+            const string W = "w";
+            const string A = "a";
+            const string S = "s";
+            const string D = "d";
+
+            bool switchReturn = true;
+
+            while (switchReturn)
+            {
+                // Part 1: takes user input for only the WASD key presses
+                //         Switch statment for processing key presses
+                Console.WriteLine("Press a WASD key and press Enter:");
+
+                string? userInput;
+                userInput = Console.ReadLine();
+                switch(userInput)
+                    {
+                    case W:
+                        Console.WriteLine("You moved up");
+                        switchReturn = false;
+                        break;
+                    case A:
+                        Console.WriteLine("You moved left");
+                        switchReturn = false;
+                        break;
+                    case S:
+                        Console.WriteLine("You moved down");
+                        switchReturn = false;
+                        break;
+                    case D:
+                        Console.WriteLine("You moved right");
+                        switchReturn = false;
+                        break;
+                    default:
+                        Console.WriteLine("Try again \n");
+                        break;
+                    }
+            }
+             
+        }
+    }
+}
